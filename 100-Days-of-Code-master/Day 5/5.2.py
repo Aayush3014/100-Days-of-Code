17900a55d706d2351918fb20c1291c2ef3c096c6
Ayush <83333424+Aayush3014@users.noreply.github.com>
2022-03-23 01:15:08 +0530

    commit

---
+++ 100-Days-of-Code-master/Day 5/5.2.py
@@ -0,0 +1,13 @@
+# For finding Max Number. 
+
+n = int(input("Number of elements you want to enter : "))
+l = []
+max = 0
+for i in range(n):
+    ele = int(input("Enter the element you want to enter : "))
+    l.append(ele)
+for j in l:
+    if j>max:
+        max = j
+else:
+    print(max)
